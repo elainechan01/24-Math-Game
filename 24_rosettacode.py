7d01d58478554b8ed862963770fdd7b3c4aeea16
elainechan01 <71743181+elainechan01@users.noreply.github.com>
2020-09-25 03:39:50 -0400

    24 Math Game Calculator
    An updated version of the 24 Math Game Calculator, derived from the Rosetta Code, python recursive version

--- 24_rosettacode.py
+++ 24_rosettacode.py
@@ -1,48 +1,33 @@
 from operator import mul, sub, add
 
 
-def div(a, b):
-    if b == 0:
-        return 999999.0
-    return a / b
-
-
-ops = {mul: '*', div: '/', sub: '-', add: '+'}
-
+def div(a, b):              
+    try:
+        x = a / b
+        return x
+    except ZeroDivisionError:
+        pass
 
 def solve24(num, how, target):
-    if len(num) == 1:
-        if round(num[0], 5) == round(target, 5):
-            yield str(how[0]).replace(',', '').replace("'", '')
-    else:
-        for i, n1 in enumerate(num):
-            for j, n2 in enumerate(num):
-                if i != j:
-                    for op in ops:
-                        new_num = [n for k, n in enumerate(num) if
-                                   k != i and k != j] + [op(n1, n2)]
-                        new_how = [h for k, h in enumerate(how) if
-                                   k != i and k != j] + [
-                                      (how[i], ops[op], how[j])]
-                        yield from solve24(new_num, new_how, target)
+    ops = {mul: '*', div: '/', sub: '-', add: '+'}
+    try:
+        if len(num) == 1:
+            if round(num[0], 5) == round(target, 5):
+                yield str(how[0]).replace(',', '').replace("'", '')
+        else:
+            for i, n1 in enumerate(num):
+                for j, n2 in enumerate(num):
+                    if i != j:
+                        for op in ops:
+                            new_num = [n for k, n in enumerate(num) if
+                                       k != i and k != j] + [op(n1, n2)]
+                            new_how = [h for k, h in enumerate(how) if
+                                       k != i and k != j] + [
+                                          (how[i], ops[op], how[j])]
+                            yield from solve24(new_num, new_how, target)
+    except TypeError:
+        return False
 
-"""
-tests = [
-    [10, 7, 2, 7],
-    [5, 7, 5, 4],
-    [1, 4, 6, 6],
-    [2, 3, 7, 3],
-    [1, 6, 2, 6],
-    [7, 9, 4, 10],
-    [6, 4, 2, 2],
-    [5, 7, 9, 7],
-    [3, 3, 8, 8],  # Difficult case requiring precise division
-    [8, 7, 9, 7],  # No solution
-    [9, 4, 4, 5],  # No solution
-]
-"""
-tests = []
-nums = []
 def get_a():
     valid_a = False
     while not valid_a:
@@ -99,20 +84,35 @@ def get_d():
             valid_d = True
             return int(d)
 
-a = get_a()
-b = get_b()
-c = get_c()
-d = get_d()
-nums.append(a)
-nums.append(b)
-nums.append(c)
-nums.append(d)
-tests.append(nums)
+def get_user_input():
+    a = get_a()
+    b = get_b()
+    c = get_c()
+    d = get_d()
+    tests = []
+    nums = []
+    nums.append(a)
+    nums.append(b)
+    nums.append(c)
+    nums.append(d)
+    tests.append(nums)
+    return tests
+
+def main():
+    recursive = True
+    while recursive:
+        tests = get_user_input()
+        for nums in tests:
+            print(nums, end=' : ')
+            try:
+                print(next(solve24(nums, nums, 24)))
+            except StopIteration:
+                print("No solution found")
+        recursive = input("Start again(Y/N)? ")
+        if recursive[0].capitalize() == "Y":
+            recursive = True
+        else:
+            recursive = False
 
 
-for nums in tests:
-    print(nums, end=' : ')
-    try:
-        print(next(solve24(nums, nums, 24)))
-    except StopIteration:
-        print("No solution found")
+print(round(15, 5))
